64e48331352b2277b216c119b572de1d831c068e
Md, Sadrul Islam <2105018@ugrad.cse.buet.ac.bd>
2024-01-27 17:41:54 +0600

    Committed after deletion completed

--- 2105018_Task1.cpp
+++ 2105018_Task1.cpp
@@ -1,256 +1,465 @@
-#include<iostream>
+#include <iostream>
 #include "color.hpp"
 using namespace std;
 
-const string R="Red";
-const string B="Black";
+const string R = "Red";
+const string B = "Black";
 
 class Node
 {
 
-    public:
-    Node* left;
-    Node* right;
-    Node* parent;
+public:
+    Node *left;
+    Node *right;
+    Node *parent;
     string color;
     string key;
     int value;
 
     Node()
     {
-        left=NULL;
-        right=NULL;
-        parent=NULL;
+        left = nullptr;
+        right = nullptr;
+        parent = nullptr;
     }
-    Node(string key,int value)
+    Node(string key, int value)
     {
-        this->value=value;
-        this->key=key;
-        left=NULL;
-        right=NULL;
-        parent=NULL;
+        this->value = value;
+        this->key = key;
+        left = nullptr;
+        right = nullptr;
+        parent = nullptr;
     }
 
-    Node(int value, Node* left, Node* right)
+    Node(int value, Node *left, Node *right)
     {
-        this->left=left;
-        this->right=right;
-        this->value=value;
+        this->left = left;
+        this->right = right;
+        this->value = value;
     }
 };
 
 class redBlackTrees
 {
-    Node* root;
-    void leftRotate(Node* node)
+    Node *root;
+    Node *Tnil;
+    void leftRotate(Node *node)
     {
-        Node* y=node->right;
-        node->right=y->left;
-        if(y->left!=NULL)
+        Node *y = node->right;
+        node->right = y->left;
+        if (y->left != Tnil)
         {
-            y->left->parent=node;
+            y->left->parent = node;
         }
-        y->parent=node->parent;
+        y->parent = node->parent;
 
-        if(node->parent==NULL)
+        if (node->parent == nullptr)
         {
-            root=y;
+            root = y;
         }
-        else if(node==node->parent->left)
+        else if (node == node->parent->left)
         {
-            node->parent->left=y;
+            node->parent->left = y;
         }
         else
         {
-            node->parent->right=y;
+            node->parent->right = y;
         }
 
-        y->left=node;
-        node->parent=y;
+        y->left = node;
+        node->parent = y;
     }
 
-    void rightRotate(Node* node)
+    void rightRotate(Node *node)
     {
-        Node* y=node->left;
-        node->left=y->right;
-        if(y->right!=NULL)
+        Node *y = node->left;
+        node->left = y->right;
+        if (y->right != Tnil)
         {
-            y->right->parent=node;
+            y->right->parent = node;
         }
-        y->parent=node->parent;
+        y->parent = node->parent;
 
-        if(node->parent==NULL)
+        if (node->parent == nullptr)
         {
-            root=y;
+            root = y;
         }
-        else if(node==node->parent->left)
+        else if (node == node->parent->left)
         {
-            node->parent->left=y;
+            node->parent->left = y;
         }
         else
         {
-            node->parent->right=y;
+            node->parent->right = y;
         }
 
-        y->right=node;
-        node->parent=y;
+        y->right = node;
+        node->parent = y;
     }
 
-    void rb_insert_fix(Node*  node)
+    void rb_insert_fix(Node *node)
     {
-        Node* y;
-       while(node->parent!=NULL && node->parent->color==R)
-       {
-          if(node->parent==node->parent->parent->left)
-          {
-            y=node->parent->parent->right;
-            if(y==NULL || y->color==B)
+        Node *y;
+        while (node->parent != nullptr && node->parent->color == R)
+        {
+            if (node->parent == node->parent->parent->left)
             {
-                if(node==node->parent->right)
+                y = node->parent->parent->right;
+                if (y == Tnil || y->color == B)
                 {
-                    node=node->parent;
-                    leftRotate(node);
+                    if (node == node->parent->right)
+                    {
+                        node = node->parent;
+                        leftRotate(node);
+                    }
+                    node->parent->color = B;
+                    node->parent->parent->color = R;
+                    rightRotate(node->parent->parent);
                 }
-                node->parent->color=B;
-                node->parent->parent->color=R;
-                rightRotate(node->parent->parent);
-            }
-            else if(y->color==R)
-            {
-                
-                node->parent->color=B;
-                node->parent->parent->color=R;
-                y->color=B;
-                node=node->parent->parent;
-            }
-          }
-
-          else
-          {
-            y=node->parent->parent->left;
-            if(y==NULL || y->color==B)
-            {
-                if(node==node->parent->left)
+                else if (y->color == R)
                 {
-                    node=node->parent;
-                    rightRotate(node);
+
+                    node->parent->color = B;
+                    node->parent->parent->color = R;
+                    y->color = B;
+                    node = node->parent->parent;
                 }
-                node->parent->color=B;
-                node->parent->parent->color=R;
-                leftRotate(node->parent->parent);
             }
 
-            else if(y->color==R)
+            else
             {
-                node->parent->color=B;
-                y->color=B;
-                node->parent->parent->color=R;
-                node=node->parent->parent;
+                y = node->parent->parent->left;
+                if (y == Tnil || y->color == B)
+                {
+                    if (node == node->parent->left)
+                    {
+                        node = node->parent;
+                        rightRotate(node);
+                    }
+                    node->parent->color = B;
+                    node->parent->parent->color = R;
+                    leftRotate(node->parent->parent);
+                }
+
+                else if (y->color == R)
+                {
+                    node->parent->color = B;
+                    y->color = B;
+                    node->parent->parent->color = R;
+                    node = node->parent->parent;
+                }
             }
-          }
-       } 
+        }
 
-       root->color=B;
+        root->color = B;
     }
 
-    void delete_fix(Node* node)
+    void delete_fix(Node *node)
     {
+        while (node!=root && node->color == B)
+        {
+            Node *w;
+            if (node == node->parent->left)
+            {
+                w = node->parent->right;
+                if (w->color == R)
+                {
+                    w->color = B;
+                    node->parent->color = R;
+                    leftRotate(node->parent);
+                    w = node->parent->right;
+                }
 
+                if (w->left->color == B && w->right->color == B)
+                {
+                    w->color = R;
+                    node = node->parent;
+                }
+
+                else
+                {
+                    if (w->right->color == B)
+                    {
+                        w->left->color = B;
+                        w->color = R;
+                        rightRotate(w);
+                        w = node->parent->right;
+                    }
+                    w->color = node->parent->color;
+                    node->parent->color = B;
+                    w->right->color=B;
+                    leftRotate(node->parent);
+                    node = root;
+                }
+            }
+            else
+            {
+                w = node->parent->left;
+                if (w->color == R)
+                {
+                    w->color = B;
+                    node->parent->color = R;
+                    rightRotate(node->parent);
+                    w = node->parent->left;
+                }
+
+                if (w->right->color == B && w->left->color == B)
+                {
+                    w->color = R;
+                    node = node->parent;
+                }
+                else
+                {
+                    if (w->left->color == B)
+                    {
+                        w->right->color = B;
+                        w->color = R;
+                        leftRotate(w);
+                        w = node->parent->left;
+                    }
+                    w->color = node->parent->color;
+                    node->parent->color = B;
+                    w->left->color=B;
+                    rightRotate(node->parent);
+                    node = root;
+                }
+            }
+        }
+
+        node->color = B;
     }
 
-    void preorderTraversal(Node* &node)
+    void transplant(Node *u, Node *v)
     {
-        if(node==NULL)
+        if (u->parent == Tnil)
+        {
+            root = v;
+        }
+
+        else if (u == u->parent->left)
+        {
+            u->parent->left = v;
+        }
+
+        else
+        {
+            u->parent->right = v;
+        }
+
+        v->parent = u->parent;
+
+        // if (v != nullptr)
+        // {
+        //     v->parent = u->parent;
+        // }
+    }
+
+    Node *inorderSuccessor(Node *node)
+    {
+        Node *temp = node;
+        while (temp->left != Tnil)
+        {
+            temp = temp->left;
+        }
+        return temp;
+    }
+
+    void preorderTraversal(Node *&node)
+    {
+        if (node == Tnil)
         {
             return;
         }
-        cout<<"("<<node->value<<" ";
-        if(node->color==R)
+        if (node->value != 0)
         {
-            cout<<dye::red(node->key)<<")"<<" ";
-        }
-        else
-        {
-            cout<<node->key<<")"<<" ";
+            cout << "(" << node->value << " ";
+            if (node->color == R)
+            {
+                cout << dye::red(node->key) << ")"
+                     << " ";
+            }
+            else
+            {
+                cout << node->key << ")"
+                     << " ";
+            }
         }
         preorderTraversal(node->left);
         preorderTraversal(node->right);
     }
-    public:
+
+public:
     redBlackTrees()
     {
-        root=NULL;
+        Tnil = new Node;
+        Tnil->color = B;
+        Tnil->left = nullptr;
+        Tnil->right = nullptr;
+        root = Tnil;
     }
 
-    void rb_insert(int value,string key)
+    void rb_insert(int value, string key)
     {
-        if(root==NULL)
+        if (root == Tnil)
         {
-            Node* node=new Node(key,value);
-            root=node;
-            root->color=B;
+            Node *node = new Node(key, value);
+            root = node;
+            root->color = B;
+            root->left = Tnil;
+            root->right = Tnil;
             return;
         }
-        Node* node=new Node(key,value);
-        Node* x=root;
-        Node* y;
-        while(x!=NULL)
+        Node *node = new Node(key, value);
+        Node *x = root;
+        Node *y = Tnil;
+        while (x != Tnil)
         {
-            y=x;
-            if(node->value<x->value)
+            y = x;
+            if (node->value < x->value)
             {
-                x=x->left;
+                x = x->left;
             }
             else
             {
-                x=x->right;
+                x = x->right;
             }
         }
 
-        node->parent=y;
+        node->parent = y;
 
-        if(node->value<y->value)
+        if (node->value < y->value)
         {
-            y->left=node;
+            y->left = node;
         }
         else
         {
-            y->right=node;
+            y->right = node;
         }
-        node->color=R;
+        node->left = Tnil;
+        node->right = Tnil;
+        node->color = R;
         rb_insert_fix(node);
+    }
+
+    void deleteNode(int value)
+    {
+        Node *z = find(value);
+        //cout<<endl;
+        if (z != Tnil)
+        {
+            Node *y = z;
+            Node *x;
+            string originColor = y->color;
+            if (z->left == Tnil)
+            {
+                x = z->right;
+                cout << z->right->value << endl;
+                transplant(z, z->right);
+            }
+
+            else if (z->right == Tnil)
+            {
+                x = z->left;
+                cout << x->value << endl;
+                transplant(z, z->left);
+            }
+
+            else
+            {
+                y = inorderSuccessor(z->right);
+                originColor = y->color;
+                x = y->right;
+                cout << y->value << endl;
+                cout << x->value << endl;
+                if (y->parent == z)
+                {
+
+                    x->parent = y;
+                }
+
+                else
+                {
+                    transplant(y, y->right);
+                    y->right = z->right;
+                    y->right->parent = y;
+                }
+
+                transplant(z, y);
+                y->left = z->left;
+                y->left->parent = y;
+
+                y->color = z->color;
+            }
+
+            delete z;
+            if (originColor == B)
+            {
+                delete_fix(x);
+            }
+        }
+    }
+
+    Node *find(int value)
+    {
+        Node *temp = root;
+        while (temp != Tnil)
+        {
+            if (value < temp->value)
+            {
+                temp = temp->left;
+            }
+
+            else if (value > temp->value)
+            {
+                temp = temp->right;
+            }
+
+            else if (value == temp->value)
+            {
+                break;
+            }
+        }
+
+        return temp;
     }
 
     void printNode()
     {
         preorderTraversal(root);
     }
+};
 
-    // void check(int flag)
-    // {
-    //     if(flag)
-    //     {
-    //         leftRotate(root);
-    //     }
-    //     else
-    //     {
-    //         rightRotate(root);
-    //     }
-    // }
+class map
+{
+    redBlackTrees R;
+
+public:
+    void deleteNode(int value)
+    {
+        if (R.find(value) != nullptr)
+        {
+            R.deleteNode(value);
+        }
+        else
+        {
+            cout << value << " Not found" << endl;
+        }
+    }
 };
 
 int main()
 {
     redBlackTrees rb;
-     rb.rb_insert(10 ,"Thors");
+    rb.rb_insert(10, "Thors");
     rb.rb_insert(34, "Canute");
-    rb.rb_insert(43,"Olaf");
-    rb.rb_insert(15,"Einer");
-    rb.rb_insert(40,"Olmar");
-    rb.rb_insert(53,"Floki");
+    rb.rb_insert(43, "Olaf");
+    rb.rb_insert(15, "Einer");
+    rb.rb_insert(40, "Olmar");
+    rb.rb_insert(53, "Floki");
+    rb.rb_insert(90, "Thorfinn");
+    rb.rb_insert(12, "Snake");
+    rb.rb_insert(78, "Askeladd");
     rb.printNode();
-
-    // rb.check(1);
-    // cout<<"After left-rotation: "<<endl;
-    // rb.printNode();
+    rb.deleteNode(40);
+    rb.printNode();
 }
